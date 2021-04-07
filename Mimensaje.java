e7178e08c8645b8e9060fdf3eba5f28795199221
josarthas <josafato007@hotmail.com>
2021-04-07 09:51:10 -0500

    ejercicio clase

---
+++ Mimensaje.java
@@ -0,0 +1,13 @@
+	public class Mimensaje {
+
+
+    public static void main(String[] args) {
+    	System.out.println("Mensaje Ejemplo");
+    	System.out.println("java".toUpperCase());
+		System.out.println("JAVA".toLowerCase());
+		System.out.println("java".length());
+		System.out.println("Mensaje Ejemplo".charAt(4));
+    }
+
+
+}
